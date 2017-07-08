1b1a3a4bd6a04402077e3c446941bdd7d7c92c24
Jonathan Lowell <elderlowell@gmail.com>
2017-07-08 13:52:00 -0600

    Completed Step 6

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const massive = require('massive');
+const controller = require('./products_controller')
 const connectionString = "postgres://jlowell@localhost/sandbox";
 
 const app = module.exports = express();
@@ -13,5 +14,11 @@ massive(connectionString)
 
 app.use( bodyParser.json() );
 app.use( cors() );
+
+app.post('/api/product', controller.create);
+app.get('/api/product/:id', controller.getOne);
+app.get('/api/products', controller.getAll);
+app.put('/api/product/:id', controller.update);
+app.delete('api/product/:id', controller.delete);
 
 app.listen( port, () => { console.log(`Listening on port ${port}.`); } );
